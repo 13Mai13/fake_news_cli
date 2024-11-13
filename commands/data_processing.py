da30c666f5caffc6c7dd4bacad37cb8188331206
mai <maialenberrondo13@gmail.com>
2024-11-13 19:41:12 +0100

    feat: metadata working

--- commands/data_processing.py
+++ commands/data_processing.py
@@ -1,14 +1,16 @@
 """
 All the commands related to data processing
 """
+
 import typer
 
 data_processing_app = typer.Typer()
 
+
 @data_processing_app.command()
 def extract_metadata(name: str = "all", path: str = ""):
     """
-    A command to bid farewell to someone, with an optional formal mode.
+    A command to extract metadata
     """
     if path & name:
         print(f"files {name} to path {path}")
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import typer
-from commands import hello_app, goodbye_app
+from commands import data_processing_app
 
 app = typer.Typer()
 
 # Adding each command as a subcommand to the main Typer app
-app.add_typer(hello_app, name="hello")
-app.add_typer(goodbye_app, name="goodbye")
+# app.add_typer(hello_app, name="hello")
+# app.add_typer(goodbye_app, name="goodbye")
+app.add_typer(data_processing_app, name="extract_metadata")
 
 if __name__ == "__main__":
     app()
